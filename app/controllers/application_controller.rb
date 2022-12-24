8ecd3e94a7e8e72cc500f75ce42cf3ef2ee6a6ca
Ludvaer <MikhailUtochkin@gmail.com>
2022-12-24 11:21:32 +0100

    localisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  before_action :set_locale
+  def set_locale
+    I18n.locale = params[:locale]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,34 @@
 Rails.application.routes.draw do
-
-
-
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #welcome
   root 'pseudo_static#welcome'
-  get 'pseudo_static/welcome', as: :pseudo_root
+  get '/:locale' => 'pseudo_static#welcome', as: :pseudo_root
 
-  #signup view destroy users
-  resources :users
-  get 'signup'  => 'users#new'
+  scope "(:locale)", locale: /en|ru/ do
+    #signup view destroy users
+    resources :users
+    get 'signup'  => 'users#new'
 
-  #login - logout
-  resources :sessions
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  get 'logout'  => 'sessions#destroy' #TODO: fix; no logout through get
-  delete 'logout'  => 'sessions#destroy'
-  get 're session'  => 'sessions#reset' #simulates session reset (reopening browser) (clears session cookie)
+    #login - logout
+    resources :sessions
+    get    'login'   => 'sessions#new'
+    post   'login'   => 'sessions#create'
+    get 'logout'  => 'sessions#destroy' #TODO: fix; no logout through get
+    delete 'logout'  => 'sessions#destroy'
+    get 're session'  => 'sessions#reset' #simulates session reset (reopening browser) (clears session cookie)
 
-  #activation stuff
-  # resources :account_activations
-  post 'account_activations' => 'account_activations#create'
-  post 'account_activations/:token' => 'account_activations#activate' , as: :account_activate
-  get 'account_activations/:token' => 'account_activations#activate' #try to avoid use get
+    #activation stuff
+    # resources :account_activations
+    post 'account_activations' => 'account_activations#create'
+    post 'account_activations/:token' => 'account_activations#activate' , as: :account_activate
+    get 'account_activations/:token' => 'account_activations#activate' #try to avoid use get
 
-  #pasword reset stuff
-  #  resources :password_resets
-  get 'reset_request'  => 'password_resets#new' #get reset request form
-  post 'send_reset_request'  => 'password_resets#create' #sends password request and creates corresponding record in db
-  get 'password_resets/:token' => 'password_resets#edit', as: :password_reset #get password reset form
-  patch 'reset_password' => 'password_resets#reset' #finally changes password in user, reset record in db can be deleted
-
+    #pasword reset stuff
+    #  resources :password_resets
+    get 'reset_request'  => 'password_resets#new' #get reset request form
+    post 'send_reset_request'  => 'password_resets#create' #sends password request and creates corresponding record in db
+    get 'password_resets/:token' => 'password_resets#edit', as: :password_reset #get password reset form
+    patch 'reset_password' => 'password_resets#reset' #finally changes password in user, reset record in db can be deleted
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
