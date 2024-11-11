993adfc0f91fa58037d1f7bfb2efc1b7561682f2
Mikhail Utochkin <MikhailUtochkin@gmail.com>
2024-11-11 17:09:19 +0100

    preparing app with preloading, code to collect elements from form and supply them to json

--- app/assets/javascripts/pick_word_in_sets.js
+++ app/assets/javascripts/pick_word_in_sets.js
@@ -1,5 +1,6 @@
 (function() {
   root = typeof exports !== "undefined" && exports !== null ? exports : this;
+
   root.fit_all_text = function (element = document)
   {
     let fit_text_svgs = element.getElementsByClassName("fit-text");
@@ -17,13 +18,83 @@
       }
     }
   }
-  root.select = function(translation_id,form_id)
+  root.select = function(translation_id,form_model)
   {
-    document.getElementById("test_pick_" + form_id + "_input").value = translation_id;
-    document.getElementById("test_pick_" + form_id + "_form").submit();
+    //document.getElementById("test_pick_" + form_id + "_input").value = translation_id;
+    form_model.picked_input.value = translation_id
+    //board = document.getElementById("optionboard_" + form_id);
+    form = form_model.form;//document.getElementById("test_pick_" + form_id + "_form")
+    //fill_pick(board, {"picked_id":translation_id})
+    form.submit();
   }
+  // function save_pick(form)
+  // {
+  //
+  // }
+  function fill_pick(form_model,data)
+  {
+    for (let j = 0; j < board.children.length; j++) {
+      let element = svg.children[j]
+      if(element.tagName == 'BUTTON')
+      {
+
+      }
+    }
+  }
+
+  serializeForm = function(form) {
+    var hash, i, item, len, ref;
+    hash = {};
+    ref = form.serializeArray();
+    for (i = 0, len = ref.length; i < len; i++) {
+      item = ref[i];
+      hash[item.name] = item.value;
+    }
+    return hash;
+  };
   // window.onload = function()
   // {
   //   root.fit_all_text(document)
   // }
+  function form_form_model_from(pick_div)
+  {
+    let model = {};
+    model.root = pick_div;
+    optionBoard = model.optionBoard = null;
+    form = model.form = null;
+    svg = model.svg = null;
+    buttons = model.buttons = [];
+    svgText = model.svgText = null;
+    picked_input = model.picked_input = null;
+    for (let j = 0; j < pick_div.children.length; j++)
+    {
+      let child = pick_div.children[j]
+      if(child.tagName == 'DIV' && child.className == 'optionboard')
+        { optionBoard = model.optionBoard = child; }
+      if(child.tagName == 'FORM')
+        { form = model.form = child; }
+    }
+    for (let j = 0; j < optionBoard.children.length; j++)
+    {
+      let child = optionBoard.children[j]
+      if(child.tagName == 'svg')
+        { svg = model.svg = child; }
+      if(child.tagName == 'BUTTON')
+        { buttons.push(child); }
+    }
+    for (let j = 0; j < svg.children.length; j++)
+    {
+      let child = svg.children[j]
+      if(child.tagName == 'TEXT')
+        { svgText = model.svgText = child; }
+    }
+    for (let j = 0; j < form.children.length; j++)
+    {
+      let child = form.children[j]
+      if(child.tagName == 'INPUT' && child.getAttribute("name") == "pick_word_in_set[picked_id]")
+        { picked_input = model.picked_input = child; }
+    }
+    return model;
+  }
+  root.form_form_model_from = form_form_model_from;
 }).call(this);
